0c43174b5d1544504628dc6921aeb7ebb210fa13
Paras-Sachdeva <paras140902@gmail.com>
2024-06-15 23:28:44 +0530

    FEAT: Check Vertically for Connect 4

--- connect4.js
+++ connect4.js
@@ -77,6 +77,18 @@ function checkWinner(){
             }
         }
     }
+
+    // Vertically
+    for(let c = 0; c < columns; c++){
+        for(let r = 0; r < rows - 3; r++){
+            if(board[r][c] != ' '){
+                if(board[r][c] == board[r+1][c] && board[r+1][c] == board[r+2][c] && board[r+2][c] == board[r+3][c]){
+                    setWinner(r, c);
+                    return;
+                }
+            }
+        }
+    }
 }
 
 function setWinner(r, c){
